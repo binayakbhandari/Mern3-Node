d9464071d6e47b0341fc730195af2a7e79bbbd56
binayakbhandari <binayakbhandari60@gmail.com>
2025-04-20 07:42:46 +0545

    Completed Day 10 and 11

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express =  require('express')
 const connectToDatabase = require('./database')
 const Blog = require('./model/blogModel')
 const app = express()
-
 app.use(express.json())
+const {multer, storage} = require('./middleware/multerConfig')
+const upload = multer({storage: storage})
 
 connectToDatabase()
 
@@ -15,8 +16,19 @@ app.get("/", (req, res)=>{
     })
 })
 
-app.post("/blog", (req, res)=>{
-    console.log(req.body)
+app.post("/blog", upload.single('image'), async (req, res)=>{
+    const {title, subtitle, description, image} = req.body
+    if(!title || !subtitle || !description){
+        return res.status(400).json({
+            message: "Please provide title, subtitle or description !"
+        })
+    }
+    await Blog.create({
+        title: title,
+        subtitle: subtitle,
+        description: description,
+        image: image
+    })
     res.status(200).json({
         message : "Blog API hit successfully !"
     })
